2a5cd90ed26740539ff997078e60f3fd7a104670
blondine-sor <144371063+blondine-sor@users.noreply.github.com>
2025-01-07 01:35:58 -0500

    Application Struction
    Definition des copesant de l'application et la page de Tutoriel

---
+++ SaferPlace/src/components/Content/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import TutorialGuide from "./Tutorial";
+
+
+
+
+function Home() {
+    return(
+        <>
+        <TutorialGuide/>
+        <h1>Welcome User</h1>
+        {/* Card to show the Motivational messages received by the server */}
+
+
+        {/* Text entry or audio download */}
+        </>
+    )
+}
+
+export default Home;
---
+++ SaferPlace/src/components/Content/Intro.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from "react";
+import { messages } from "../../data/messages";
+
+
+
+function Introduction(){
+
+   
+}
+
+export default Introduction
